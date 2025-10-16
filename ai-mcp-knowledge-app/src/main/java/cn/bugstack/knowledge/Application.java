1a5268c286eff9c6e088c0e75931e64e30b686b5
yangjian <525919113@qq.com>
2025-10-16 15:29:21 +0800

    11：吃上细糠，升级SpringAI框架1.0.0-M6

---
+++ ai-mcp-knowledge-app/src/main/java/cn/bugstack/knowledge/Application.java
@@ -0,0 +1,17 @@
+package cn.bugstack.knowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: yangjian
+ * @Date: 2025-10-16 - 10 - 16 - 10:18
+ * @Description: cn.bugstack.knowledge
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
